90c4bb7113aa5affc142f68d83de58397aa2dc11
justinrezie <seourngsauphorn@gmail.com>
2025-01-15 21:18:21 +0700

    New add

--- index.php
+++ index.php
@@ -23,6 +23,7 @@ function fetchCategory($conn, $category_id) {
     return mysqli_fetch_assoc($category_result);
 }
 ?>
+
 <!-- FEATURED POST -->
 <?php if (mysqli_num_rows($featured_result) == 1) : ?>
 <section class="featured">
